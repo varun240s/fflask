c04ff5cbfbc2286dd7d243a2d6384a16516deb96
varun240s <reddyvarun240@gmail.com>
2025-08-25 00:02:13 +0530

    updated CRUD operations

--- myProject/hello.py
+++ myProject/hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy 
-from flask import send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask import send_from_directory, request, redirect, url_for
 from datetime import datetime
 
 
@@ -21,19 +21,48 @@ class Todo(db.Model):
         return f"Todo('{self.sno}', '{self.title}', '{self.date_created}')"
 
 @app.route('/')
-def home():
-    todo = Todo(title="first Todo", desc = "Start investing in stock market")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template('index.html')
+def hello_world():
+    all_Todo = Todo.query.all()
+    return render_template('index.html', allTodo=all_Todo)
 
-@app.route('/projects/')
+@app.route('/show')
 def projects():
-    return 'The project page'
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return "The Todo's Page page"
 
 @app.route('/about/')
 def about():
     return 'The about page'
+
+# Create Todo
+@app.route('/add', methods=['POST'])
+def add_todo():
+    title = request.form['title']
+    desc = request.form['desc']
+    todo = Todo(title=title, desc=desc)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('hello_world'))
+
+# Update Todo
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update_todo(sno):
+    todo = Todo.query.get_or_404(sno)
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect(url_for('hello_world'))
+    return render_template('update.html', todo=todo)
+
+# Delete Todo
+@app.route('/delete/<int:sno>')
+def delete_todo(sno):
+    todo = Todo.query.get_or_404(sno)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('hello_world'))
     
 @app.route('/download')
 def download_pdf():
